a1fea2493c7ee2decc1701566df09303ebf6bc8a
Chingis <chingisdev@gmail.com>
2021-09-07 16:07:19 +0300

    fixed

--- task_3/factorial.js
+++ task_3/factorial.js
@@ -1,10 +1,16 @@
 const factorial = (number) => {
-    if (number === 1) {
+    if (typeof number != "number") {
+        return;
+    }
+    if (number <= 1) {
         return number;
     }
     return number * factorial(number - 1);
 }
 
+
+console.log(factorial(0), '0');
+console.log(factorial(1), '1');
 console.log(factorial(4), '24');
 console.log(factorial(7), '5040');
 console.log(factorial(17), '355687428096000');
